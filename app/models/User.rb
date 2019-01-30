214b3c7e08172e5f3c96399215185d92082f0f1b
Anton Westman <antonwestman@gmail.com>
2019-01-30 12:47:48 +0100

    Pesky filesystem

--- app/models/User.rb
+++
@@ -1,7 +0,0 @@
-class User < ApplicationRecord
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
-         :confirmable, :omniauthable
-
-  include DeviseTokenAuth::Concerns::User
-end
