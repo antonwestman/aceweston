efdba4259c61e1f42ba5c59278a80034db2d83c4
Anton Westman <antonwestman@gmail.com>
2019-01-30 12:42:37 +0100

    Grip for straws

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActiveSupport.on_load(:after_initialize) do
+  ArkivITInitialized = true
+  ArkivIT::Application.reload_routes! # this time with devise
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
+  if defined?(ArkivITInitialized)
+    mount_devise_token_auth_for 'User', at: 'auth'
+  end
 
   root 'main#index'
   get '*path', to: 'main#index' # Route catch-all to root
