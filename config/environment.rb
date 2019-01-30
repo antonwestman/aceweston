e35a983922f3468fea44e7fec03f477ee8aa6c2b
Anton Westman <antonwestman@gmail.com>
2019-01-30 12:49:49 +0100

    Put straws back

--- config/environment.rb
+++ config/environment.rb
@@ -3,8 +3,3 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-ActiveSupport.on_load(:after_initialize) do
-  ArkivITInitialized = true
-  ArkivIT::Application.reload_routes! # this time with devise
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  if defined?(ArkivITInitialized)
-    mount_devise_token_auth_for 'User', at: 'auth'
-  end
+  mount_devise_token_auth_for 'User', at: 'auth'
 
   root 'main#index'
   get '*path', to: 'main#index' # Route catch-all to root
