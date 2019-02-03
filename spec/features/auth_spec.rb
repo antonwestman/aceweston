ede894940804d3061555b26faac66fc1b6cc5a5a
Anton Westman <antonwestman@gmail.com>
2019-02-03 01:57:13 +0100

    Fix failing tests

--- spec/features/auth_spec.rb
+++ spec/features/auth_spec.rb
@@ -5,14 +5,14 @@ describe 'Signup Flow', type: :feature, js: true do
   end
   context 'Signing in' do
     before {
-      page.find('a', text: 'Sign in').click
+      page.find('a', text: 'Logga in').click
     }
     it 'brings them login page' do
       expect(page).to have_current_path('/login')
     end
   end
   context 'Signing up' do
-    before { page.find('a', text: 'Sign up').click }
+    before { page.find('a', text: 'Skapa ett konto').click }
     it 'brings them to register page' do
       expect(page).to have_current_path('/register')
     end
